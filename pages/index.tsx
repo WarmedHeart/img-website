3b3a1d685ce0a2bcf93141350aede682051204c7
thang <16thguo@gmail.com>
2024-06-15 19:33:38 +0800

    feature: 首页图片列表

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,43 @@
-import { useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import Head from "next/head";
+import { Photo } from "react-photo-album";
 
 import PhotoList from "@/components/business/photo-list";
 import TopBanner from "@/components/business/top-banner";
 
+import { getPhotoListBySearch } from "@/services/pages/index";
+
 import styles from "@/styles/index.module.scss";
 
 export default function Home() {
-
-  const wrapperRef = useRef(null);
   
+  // 关键字
+  const [searchKey, setSearchKey] = useState('');
+  // 标签
+  const [labelList, setLabelList] = useState([]);
+
+  // 分页数据
+  const [pageNum, setPageNum] = useState(0);
+  const [photos, setPhotos] = useState<Array<Photo>>([]);
+
+  useEffect(() => {
+    getPhotoByKeyAndPage({searchKey, labelList, pageNum}, false);
+  }, [searchKey, labelList]);
+
+
+  const getPhotoByKeyAndPage = async (params: {searchKey?: string, labelList?: Array<string>, pageNum: number}, isAppend: boolean) => {
+    const resp = await getPhotoListBySearch(params);
+    setPhotos(isAppend ? [...photos, ...resp] : resp);
+
+    if (params.pageNum === 5) {
+      return false;
+    }
+
+    return true;
+  }
+
   return (
-    <div className={styles['main-page-container']} ref={wrapperRef}>
+    <div className={styles['main-page-container']}>
       <Head>
         <title>自由图片网</title>
         <meta
@@ -26,7 +52,9 @@ export default function Home() {
       />
       {/* 简略版图片排列 */}
       <PhotoList
+        value={photos}
         wrapperClass={styles['context']}
+        onNextPhotos={(pageNum) => getPhotoByKeyAndPage({searchKey, labelList, pageNum}, true)}
       />
     </div>
   );
